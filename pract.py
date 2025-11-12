f805eb285f41d406e160736707c34172a1d9eeeb
HBJ17 <hbj17672007@gmail.com>
2025-11-12 10:47:37 +0530

    added a practical file

--- pract.py
+++ pract.py
@@ -1,8 +1,10 @@
 import pandas as pd
-data = pd.read_csv("D:\Studies\Projects\Python\AI-ML-DL\data\chatgpt_cleaned.csv")
+data = pd.read_csv("D:\Studies\Projects\Python\AI-ML-DL\data\chatgpt_cleaned.csv", index_col=0)
 print(data.head())
 print()
 print(data.info())
 print()
 print(data.describe())
+print(data.shape)
+
 
