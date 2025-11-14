924f44538eac6ee96bb2af1253d3d8b44b846c6c
HBJ17 <hbj17672007@gmail.com>
2025-11-14 16:15:18 +0530

    creating a data frame with index

--- Pandas/Creating a data frame with index.py
+++ Pandas/Creating a data frame with index.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-# Create DataFrame
 data = {'Name': ['Asha', 'Bala', 'Catherine'],
         'Age': [20, 22, 19],
         'Score': [88, 92, 95]}
-df = pd.DataFrame(data)
+df = pd.DataFrame(data , index = [1,2,3])
 
 print(df)
-print("\nAverage Score:", df['Score'].mean())
+mean = df['Score'].mean()
+print(f"\nAverage Score: {mean:.2f}")
+
